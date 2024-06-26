412e09fbd95d8a12dac2359f5ce20889496743a9
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-06-26 13:09:28 -0300

    Create arvore_B.h

---
+++ Arvore_B/arvore_B.h
@@ -0,0 +1,51 @@
+#ifndef INC_13_TRABALHO2_ARVORE_B_H
+#define INC_13_TRABALHO2_ARVORE_B_H
+
+/*** -- Estruturas -- ***/
+
+typedef struct pagina pagina;
+
+typedef struct chave chave;
+
+typedef struct arvore arvore;
+
+/*** -- Funções -- ***/
+
+/*
+Descrição: Cria uma estrutura da árvore b.
+Entrada: Inteiro da ordem da árvore.
+Saída: Ponteiro para a árvore; NULL em caso de erro.
+*/
+arvore *criaArvore(int ordem);
+
+/*
+Descrição: Retorna a raiz da árvore.
+Entrada: Ponteiro para a árvore b.
+Saída: Ponteiro para a raiz da árvore.
+*/
+pagina *getRaiz(arvore *arv);
+
+/*
+Descrição: Cria uma estrutura da página.
+Entrada: Ponteiro para a árvore b.
+Saída: Ponteiro para a página; NULL em caso de erro.
+*/
+pagina *criaPagina(arvore *arv);
+
+/*
+Descrição: Insere um elemento na árvore, e chama as funções de correção dependendo do caso.
+Entrada: Ponteiro para a árvore b, inteiro da chave, inteiro da linha.
+Saída: 1 - Sucesso, 0 - Erro.
+*/
+int insereNo(arvore *arv, int valor, int indice);
+
+/*
+Descrição: Aloca uma nova estrutura de chave, e salva ela na lista de chaves da página.
+Entrada: Ponteiro para a página onde será inserido, inteiro da chave, inteiro da linha.
+Saída: 1 - Sucesso, 0 - Erro.
+*/
+int insereFolha(pagina *page, int valor, int indice);
+
+
+
+#endif //INC_13_TRABALHO2_ARVORE_B_H
