cc5615cb24460b5f5fb0da2819a8f46599f6db90
gebra04 <gebramurilo@gmail.com>
2024-06-27 14:48:52 -0300

    Funções de Remoção

--- Arvore_B/arvore_B.c
+++ Arvore_B/arvore_B.c
@@ -3,23 +3,98 @@
 
 /*** -- Estruturas -- ***/
 
+struct chave{
+    int chave;
+    int indice;
+};
+
 struct pagina{
-    int *chaves;
-    int *indice;
+    chave **chaves;
     int nChaves;
     int folha;
-    pagina *filhos;
+    pagina **filhos;
     pagina *pai;
 };
 
 struct arvore{
-    pagina *sentinela;
+    pagina *raiz;
     int numElementos;
     int ordem;
 };
 
 /*** -- Funções -- ***/
 
+void merge(pagina *page, arvore *arv, int idx){
+    int i = 0;
+    // Idx indica a posição do pai direto entre as paginas do merge
+    pagina *filho = page->filhos[idx];
+    pagina *irmao = page->filhos[idx + 1];
+
+    filho->chaves[filho->nChaves] = page->chaves[idx];
+    while(filho->nChaves){
+        // Move as chaves do irmão para o outro (filho).
+        irmao->chaves[irmao->nChaves+i] = filho->chaves[i];
+        filho->nChaves--;
+        i++;
+    }
+    if(page->pai->nChaves < arv->ordem%2)
+        merge(page->pai, arv, idx);
+}
+
+// Função recursiva para buscar a chave na árvore B
+pagina *buscaChave(struct pagina *page, int chave) {
+    int i = buscaIdx(page, chave);
+    // Se a chave é encontrada nesta página, retorna a página
+    if (i < page->nChaves && page->chaves[i]->chave == chave) {
+        return page;
+    }   
+    // Se a página é uma folha, a chave não está presente
+    if (page->folha) {
+        return NULL;
+    }    
+    // Se a página não é uma folha, busca recursivamente no filho apropriado
+    return buscar(page->filhos, chave);
+}
+
+// Função para encontrar a chave antecessora 
+chave *encontraAntecessor(pagina *pagina) {
+    while (!pagina->folha) {
+        pagina = pagina->filhos[pagina->nChaves];
+    }
+    return pagina->chaves[pagina->nChaves - 1];
+}
+
+
+// Função auxiliar para buscar o idx de uma página
+int buscaIdx(pagina *pagina, int chave) {
+    int idx = 0;
+    while (idx < pagina->nChaves && chave > pagina->chaves[idx]->chave) {
+        idx++;
+    }
+    return idx;
+}
+
+void removeDeFolha(pagina *page, int idx){
+    for(idx ; idx < page->nChaves ; idx++){
+        page->chaves[idx] = page->chaves[idx+1];
+    }
+    page->chaves[idx] = NULL;
+    page->nChaves--;
+    return;
+}
+
+void removeDeNaoFolha(pagina *page, int idx){
+    int k = page->chaves[idx]->chave;
+    page->chaves[idx]->chave = encontraAntecessor(page);
+    removeDeFolha(buscaChave(page, page->chaves[idx]->chave), buscaIdx(page, k));
+    return;
+}
+
+/*
+Descrição: Cria uma estrutura da árvore b.
+Entrada: Inteiro da ordem da árvore.
+Saída: Ponteiro para a árvore; NULL em caso de erro.
+*/
 arvore *criaArvore(int ordem){
     arvore *arv = (arvore*)malloc(sizeof(arvore));
     if(!arv){
@@ -27,56 +102,43 @@ arvore *criaArvore(int ordem){
     }
     arv->numElementos = 0;
     arv->ordem = ordem;
-    arv->sentinela = (pagina*)malloc(sizeof(pagina));
-    if(!arv->sentinela){
-        free(arv);
-        return NULL;
-    }
-    arv->sentinela->chaves = (int*)malloc(sizeof(int));
-    if(!arv->sentinela->chaves){
-        free(arv->sentinela);
-        free(arv);
-        return NULL;
-    }
-    arv->sentinela->chaves[0] = -1000;
-    arv->sentinela->indice = (int*)malloc(sizeof(int));
-    if(!arv->sentinela->indice){
-        free(arv->sentinela->chaves);
-        free(arv->sentinela);
-        free(arv);
-        return NULL;
-    }
-    arv->sentinela->indice[0] = -1;
-    arv->sentinela->folha = 0;
-    arv->sentinela->nChaves = 1;
-    arv->sentinela->filhos = NULL;
-    arv->sentinela->pai = NULL;
+    arv->raiz = NULL;
     return arv;
 }
 
-pagina *criaPagina(arvore *arv){
+/*
+Descrição: Retorna a raiz da árvore.
+Entrada: Ponteiro para a árvore b.
+Saída: Ponteiro para a raiz da árvore.
+*/
+pagina *getRaiz(arvore *arv){
+    return arv->raiz;
+}
+
+/*
+Descrição: Cria uma estrutura da página.
+Entrada: Ponteiro para a árvore b.
+Saída: Ponteiro para a página; NULL em caso de erro.
+*/
+pagina *criapagina(arvore *arv){
     pagina *page = (pagina*)malloc(sizeof(pagina));
     if(!page){
         return NULL;
     }
     if(arv->ordem % 2 == 0){
-        page->chaves = (int*)malloc(sizeof(int) * (arv->ordem - 1));
+        page->chaves = (chave**)malloc(sizeof(chave*) * (arv->ordem - 1));
     }else{
-        page->chaves = (int*)malloc(sizeof(int) * (arv->ordem));
+        page->chaves = (chave**)malloc(sizeof(chave*) * (arv->ordem));
     }
     if(!page->chaves){
         free(page);
         return NULL;
     }
-    if(arv->ordem % 2 == 0){
-        page->indice = (int*)malloc(sizeof(int) * (arv->ordem - 1));
-    }else{
-        page->indice = (int*)malloc(sizeof(int) * (arv->ordem));
+    for(int i = 0; i < arv->ordem-1; i++){
+        page->chaves[i] = NULL;
     }
-    if(!page->indice){
-        free(page->chaves);
-        free(page);
-        return NULL;
+    if(arv->ordem % 2 == 1){
+        page->chaves[arv->ordem-1] = NULL;
     }
     page->pai = NULL;
     page->filhos = NULL;
@@ -85,18 +147,76 @@ pagina *criaPagina(arvore *arv){
     return page;
 }
 
-int insereNo(arvore *arv, int chave, int indice){
+/*
+Descrição: Insere um elemento na árvore, e chama as funções de correção dependendo do caso.
+Entrada: Ponteiro para a árvore b, inteiro da chave, inteiro da linha.
+Saída: 1 - Sucesso, 0 - Erro.
+*/
+int insereNo(arvore *arv, int valor, int indice){
+    pagina *pageAtual, *pageAux; //pageAtual percorre a árvore, pageAux auxilia salvando o pai.
+    //Verificando se a árvore está vazia, se estiver apenas cria uma página e vincula com a raiz, se não estiver procure a página.
     if(!arv->numElementos){
-        arv->sentinela->filhos = criaPagina(arv);
-        if(!arv->sentinela->filhos){
+        arv->raiz = criapagina(arv);
+        if(!arv->raiz){
             return 0;
         }
-        arv->sentinela->filhos->chaves[0] = chave;
-        arv->sentinela->filhos->indice[0] = indice;
-        arv->sentinela->filhos->nChaves = 1;
-        arv->sentinela->filhos->pai = arv->sentinela;
+        arv->raiz->chaves[0] = (chave*)malloc(sizeof(chave));
+        if(!arv->raiz->chaves[0]){
+            free(arv->raiz);
+            return 0;
+        }
+        arv->raiz->chaves[0]->chave = valor;
+        arv->raiz->chaves[0]->indice = indice;
+        arv->raiz->nChaves = 1;
     }else{
-        //Sinistro
+        pageAtual = arv->raiz;
+        //Enquanto o pageAtual não chegar na folha, continue procurando.
+        while(!pageAtual->folha){
+            pageAux = pageAtual; //Salvando o pai.
+            //Verificando em qual filho entrar.
+            for(int i = 0; i < pageAux->nChaves && pageAtual == pageAux; i++){
+                if(valor < pageAux->chaves[i]->chave){
+                    pageAtual = pageAux->filhos[i];
+                }else if(i == pageAux->nChaves - 1){
+                    pageAtual = pageAux->filhos[i+1];
+                }
+            }
+        }
+        //Verificando se a página atual está cheia, se não estiver insere a nova chave, se estiver split.
+        if(pageAtual->nChaves < arv->ordem - 1){
+            if(!insereFolha(pageAtual, valor, indice)){
+                return 0;
+            }
+        }else{
+            //Nó cheio
+            if(pageAtual->pai || pageAtual->pai->nChaves == arv->ordem - 1){
+                //Pai cheio
+            }else{
+                //Tem espaço no pai
+            }
+        }
     }
+    return 1;
+}
+
+/*
+Descrição: Aloca uma nova estrutura de chave, e salva ela na lista de chaves da página.
+Entrada: Ponteiro para a página onde será inserido, inteiro da chave, inteiro da linha.
+Saída: 1 - Sucesso, 0 - Erro.
+*/
+int insereFolha(pagina *page, int valor, int indice){
+    int pos = page->nChaves - 1;
+    page->chaves[pos + 1] = (chave*)malloc(sizeof(chave));
+    if(!page->chaves[pos + 1]){
+        return 0;
+    }
+    while(pos >= 0 && valor < page->chaves[pos]->chave){
+        page->chaves[pos+1]->chave = page->chaves[pos]->chave;
+        page->chaves[pos+1]->indice = page->chaves[pos]->indice;
+        pos--;
+    }
+    page->chaves[pos + 1]->chave = valor;
+    page->chaves[pos + 1]->indice = indice;
+    page->nChaves+=1;
     return 1;
 }
--- Arvore_B/arvore_B.h
+++ Arvore_B/arvore_B.h
@@ -1,10 +1,57 @@
+#ifndef INC_13_TRABALHO2_ARVORE_B_H
+#define INC_13_TRABALHO2_ARVORE_B_H
+
 /*** -- Estruturas -- ***/
 
 typedef struct pagina pagina;
 
+typedef struct chave chave;
+
 typedef struct arvore arvore;
 
 /*** -- Funções -- ***/
+
+/*
+Descrição: Função para reorganizar a árvore por merge dos irmãos.
+Entrada: Ponteiro para página, ponteiro para árvore e 
+Saída: Ponteiro para a maior chave da subárvore esquerda.
+*/
+void merge(pagina *page, arvore *arv, int idx);
+
+/*
+Descrição: Função para encontrar a maior chave na subárvore esquerda.
+Entrada: Ponteiro para página
+Saída: Ponteiro para a maior chave da subárvore esquerda.
+*/
+chave *encontraAntecessor(pagina *pagina);
+
+/*
+Descrição: Função auxiliar para buscar o índice de uma página.
+Entrada: Ponteiro para página e a chave a ser buscada.
+Saída: Índice para a chave procurada.
+*/
+int buscaChave(pagina *pagina, int chave);
+
+/*
+Descrição: Função auxiliar para buscar a posição de um elemento na página.
+Entrada: Ponteiro para página e a chave a ser buscada.
+Saída: Posição para a chave procurada.
+*/
+int buscaIdx(pagina *pagina, int chave);
+
+/*
+Descrição: Função para remover chave de folha.
+Entrada: Ponteiro para página e a chave a ser removida.
+Saída: Void.
+*/
+void removeDeFolha(pagina *page, int indice);
+
+/*
+Descrição: Função para remover chave de não folha.
+Entrada: Ponteiro para página e a chave a ser removida.
+Saída: Void.
+*/
+void removeDeNaoFolha(pagina *page, int i);
 
 /*
 Descrição: Cria uma estrutura da árvore b.
@@ -12,6 +59,13 @@ Entrada: Inteiro da ordem da árvore.
 Saída: Ponteiro para a árvore; NULL em caso de erro.
 */
 arvore *criaArvore(int ordem);
+
+/*
+Descrição: Retorna a raiz da árvore.
+Entrada: Ponteiro para a árvore b.
+Saída: Ponteiro para a raiz da árvore.
+*/
+pagina *getRaiz(arvore *arv);
 
 /*
 Descrição: Cria uma estrutura da página.
@@ -26,3 +80,7 @@ Entrada: Ponteiro para a árvore b, inteiro da chave, inteiro da linha.
 Saída: 1 - Sucesso, 0 - Erro.
 */
 int insereNo(arvore *arv, int chave, int indice);
+
+
+
+#endif //INC_13_TRABALHO2_ARVORE_B_H
