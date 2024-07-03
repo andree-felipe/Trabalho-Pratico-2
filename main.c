683595c5d63d7d87120561cc9d67cb21d75c1d07
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-07-03 10:19:21 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 #include "Arvore_B/arvore_B.h"
-#include "Entrada_Saida/ES.h"
+
+/*** - Estruturas -- ***/
 
 typedef struct registro {
     int matricula;
@@ -17,98 +17,154 @@ typedef struct metricas {
     float media;
 }metricas;
 
+/*** -- Funções -- ***/
+
+/*
+Descrição: Função responsável por pegar o índice e com o tamanho da linha achar a linha com o valor passado.
+Entrada: Inteiro do índice, ponteiro para o arquivo.
+Saída: Nada.
+*/
 void buscaArquivoIndice(int indice, FILE *arq){
     registro dadosColetados;
     signed long tamanhoLinha = sizeof(char[35]);
-
-    fseek(arq, tamanhoLinha * indice, SEEK_SET);
+    fseek(arq, tamanhoLinha * indice, SEEK_SET); //Colocando o ponteiro do arquivo na linha do registro.
     fscanf(arq, "%d %s %s %ld", &dadosColetados.matricula, dadosColetados.nome, dadosColetados.dataNasc, &dadosColetados.cpf);
-    printf("Nome: %s | Matricula: %04d | Data de Nascimento: %s | CPF: %011ld\n", dadosColetados.nome, dadosColetados.matricula, dadosColetados.dataNasc, dadosColetados.cpf);
+    printf("\nNome: %s | Matricula: %04d | Data de Nascimento: %s | CPF: %011ld\n", dadosColetados.nome, dadosColetados.matricula, dadosColetados.dataNasc, dadosColetados.cpf);
 }
 
+/*
+Descrição: Função responsável por fazer a busca direta no arquivo, ela lê o arquivo até achar o registro correspondente.
+Entrada: Inteiro da matrícula, ponteiro para o arquivo.
+Saída: Nada.
+*/
 void buscaArquivoDireto(int mat, FILE *arq){
     registro dadosColetados;
-
-    rewind(arq);
+    rewind(arq); //Colocando o ponteiro do arquivo no início.
     dadosColetados.matricula = -1;
     while(!feof(arq) && dadosColetados.matricula != mat) {
         fscanf(arq, "%d %s %s %ld", &dadosColetados.matricula, dadosColetados.nome, dadosColetados.dataNasc, &dadosColetados.cpf);
         if(dadosColetados.matricula == mat) {
-            printf("Nome: %s | Matricula: %04d | Data de Nascimento: %s | CPF: %011ld\n", dadosColetados.nome, dadosColetados.matricula, dadosColetados.dataNasc, dadosColetados.cpf);
+            printf("\nNome: %s | Matricula: %04d | Data de Nascimento: %s | CPF: %011ld\n", dadosColetados.nome, dadosColetados.matricula, dadosColetados.dataNasc, dadosColetados.cpf);
         }
     }
 }
 
+/*
+Descrição: Função principal, responsável por pedir as entradas do usuário, e chamar as funções de busca e da árvore.
+Entrada: Nada.
+Saída: 0 - Sucesso, 1 - Erro.
+*/
 int main(void) {
     FILE *arq;
     arvore *arv;
-    pagina *page;
-    registro reg;
     metricas btree, direto;
     clock_t inicio, fim;
     int resp, indice, mat, qtdPesquisa=10000;
     double tempo;
     char nomeArquivoResultados[20], nomeArquivo[20] = "entrada.txt";
 
-    //printf("\nQual será a ordem da árvore?\nResposta: ");
-    //scanf("%d", &resp);
-
-    arv = criaArvore(4);
-    if(!arv){
-        printf("Erro na criação do arquivo de índice!");
+    //Pedindo para o usuário digitar a ordem da árvore.
+    printf("\nQual será a ordem da árvore? (Somente ordens pares).\nResposta: ");
+    scanf("%d", &resp);
+    if(resp % 2 != 0){
+        printf("\nOrdem não par!");
         return 1;
     }
 
-    arq = fopen(nomeArquivo, "r");
+    //Criando a árvore b com a ordem digitada.
+    arv = criaArvore(resp);
+    if(!arv){
+        printf("\nErro na criação da árvore!");
+        return 1;
+    }
 
+    //Lendo o arquivo informado.
+    arq = fopen(nomeArquivo, "r");
+    if(!arq){
+        printf("\nErro na abertura do arquivo.");
+        return 1;
+    }
+
+    //Iniciando o menu.
     do{
         printf("\nMenu\n1. Criar índice\n2. Procurar elementos\n3. Remover registro\n4. Sair\n\nEscolha uma opção: ");
         scanf("%d", &resp);
         switch (resp){
             case 1:
+                //Todos as entradas no arquivo serão jogadas para a árvore.
                 if(!processaEntrada(arv, arq)) {
-                    printf("Erro na abertura do arquivo!");
+                    printf("\nErro na inserção da árvore.");
                     return 1;
                 }
                 break;
             case 2:
-                btree.tMin = btree.tMax = btree.media = 0;
-                direto.tMin = direto.tMax = direto.media = 0;
-                srand(time(NULL));
-                for(int i = 0; i < qtdPesquisa; i++){
-                    mat = rand() % 11000;
+                printf("\n1. Busca específica\n2. Busca valores aleatórios\n\nEscolha uma opção: ");
+                scanf("%d", &resp);
+                if(resp == 1){
+                    printf("\nInforme a matrícula que você deseja buscar: ");
+                    scanf("%d", &mat);
+                    //Início da busca na B-Tree.
                     inicio = clock();
                     indice = buscaChave(getRaiz(arv), mat);
                     buscaArquivoIndice(indice, arq);
                     fim = clock();
+                    //Fim da busca na B-Tree.
                     tempo = (double)(fim-inicio)/(CLOCKS_PER_SEC/1000);
-                    btree.media += tempo;
-                    if(btree.tMin == 0 && btree.tMax == 0)
-                        btree.tMin = btree.tMax = tempo;
-                    else if(tempo < btree.tMin)
-                        btree.tMin = tempo;
-                    else if(tempo > btree.tMax)
-                        btree.tMax = tempo;
-                    
+                    printf("Tempo para a busca por índice: %f\n", tempo);
+                    //Início da busca direta.
                     inicio = clock();
                     buscaArquivoDireto(mat, arq);
                     fim = clock();
+                    //Fim da busca direta.
                     tempo = (double)(fim-inicio)/(CLOCKS_PER_SEC/1000);
-                    direto.media += tempo;
-                    if(direto.tMin == 0 && direto.tMax == 0)
-                        direto.tMin = direto.tMax = tempo;
-                    else if(tempo < direto.tMin)
-                        direto.tMin = tempo;
-                    else if(tempo > direto.tMax)
-                        direto.tMax = tempo;
-                    
+                    printf("Tempo para a busca direta: %f\n", tempo);
+                }else{
+                    //Setando as métricas como 0.
+                    btree.tMin = btree.tMax = btree.media = 0;
+                    direto.tMin = direto.tMax = direto.media = 0;
+                    //Gerando uma nova seed.
+                    srand(time(NULL));
+                    //Fazendo as n pesquisas aleatórias.
+                    for(int i = 0; i < qtdPesquisa; i++){
+                        mat = rand() % 11000;
+                        //Início da busca na B-Tree.
+                        inicio = clock();
+                        indice = buscaChave(getRaiz(arv), mat);
+                        buscaArquivoIndice(indice, arq);
+                        fim = clock();
+                        //Fim da busca na B-Tree.
+                        //Calculando o tempo, e salvando nas variáveis.
+                        tempo = (double)(fim-inicio)/(CLOCKS_PER_SEC/1000);
+                        btree.media += tempo;
+                        if(btree.tMin == 0 && btree.tMax == 0)
+                            btree.tMin = btree.tMax = tempo;
+                        else if(tempo < btree.tMin)
+                            btree.tMin = tempo;
+                        else if(tempo > btree.tMax)
+                            btree.tMax = tempo;
+                        //Início da busca direta.
+                        inicio = clock();
+                        buscaArquivoDireto(mat, arq);
+                        fim = clock();
+                        //Fim da busca direta.
+                        //Calculando o tempo, e salvando nas variáveis.
+                        tempo = (double)(fim-inicio)/(CLOCKS_PER_SEC/1000);
+                        direto.media += tempo;
+                        if(direto.tMin == 0 && direto.tMax == 0)
+                            direto.tMin = direto.tMax = tempo;
+                        else if(tempo < direto.tMin)
+                            direto.tMin = tempo;
+                        else if(tempo > direto.tMax)
+                            direto.tMax = tempo;
+                    }
+                    //Calculando a média de ambos os casos.
+                    btree.media /= (float)qtdPesquisa;
+                    direto.media /= (float)qtdPesquisa;
+                    //Salvando em um arquivo.
+                    printf("Nome do arquivo: ");
+                    scanf(" %[^\n]", nomeArquivoResultados);
+                    geraArquivoResultados(nomeArquivoResultados, btree, direto);
                 }
-                btree.media /= (float)qtdPesquisa;
-                direto.media /= (float)qtdPesquisa;
-
-                printf("Nome do arquivo: ");
-                scanf(" %[^\n]", nomeArquivoResultados);
-                geraArquivoResultados(nomeArquivoResultados, btree, direto);
                 break;
             case 3:
                 break;
@@ -116,7 +172,7 @@ int main(void) {
                 break;
         }
     }while(resp != 4);
-
+    free(arv);
     fclose(arq);
     return 0;
 }
