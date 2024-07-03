888b6dbe11e0a467900c44b4108797169fefc707
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-07-03 20:47:55 -0300

    Update arvore_B.c

--- Arvore_B/arvore_B.c
+++ Arvore_B/arvore_B.c
@@ -356,3 +356,249 @@ int processaEntrada(arvore *arv, FILE *arq){
     }
     return 1;
 }
+
+/*
+Descrição: Função que libera a memória alocada para uma página.
+Entrada: Página que irá ser liberada.
+Saída: Nada.
+*/
+void deletaPagina(pagina *page){
+    free(page->chaves);
+    free(page->filhos);
+    free(page);
+}
+
+/*
+Descrição: Função que procura o predecessor da página, e copía ele para a posição informada.
+Entrada: Ponteiro para a página que irá receber o predecessor, inteiro da posição onde o predecessor será copiado.
+Saída: Nada.
+*/
+void copiaPredecessor(pagina *page, int pos){
+    pagina *filho = page->filhos[pos]; //Pegando o filho da esquerda (elementos menores do que a chave).
+    //Enquanto o filho não for folha, vamos para a sub-árvore mais a direita.
+    while(!filho->folha){
+        filho = filho->filhos[filho->nChaves];
+    }
+    //Fazendo a cópia para a posição informada.
+    page->chaves[pos]->chave = filho->chaves[filho->nChaves-1]->chave;
+    page->chaves[pos]->indice = filho->chaves[filho->nChaves-1]->indice;
+}
+
+/*
+Descrição: Função que remove o elemento indicado na página informada.
+Entrada: Ponteiro para a página folha onde o elemento será removido, inteiro da posição onde está o elemento.
+Saída: Nada.
+*/
+void removeChave(pagina *page, int pos){
+    chave *aux = page->chaves[pos]; //Salvando o bloco que será removido, para liberar depois.
+    //Fazendo com que todos os elementos à direita da posição, deem um shift para a esquerda.
+    for(int i = pos; i < page->nChaves - 1; i++){
+        page->chaves[i] = page->chaves[i + 1];
+    }
+    //Setando a última chave como nula.
+    page->chaves[page->nChaves - 1] = NULL;
+    //Liberando o bloco, e setando o novo tamanho da folha.
+    free(aux);
+    page->nChaves--;
+}
+
+/*
+Descrição: Função que realiza o processo de rotação para a esquerda na página.
+Entrada: Ponteiro para a página pai, inteiro indicando qual filho receberá o novo elemento.
+Saída: Nada.
+*/
+void emprestaDireita(pagina *page, int pos){
+    pagina *filhoD = page->filhos[pos + 1], *filhoE = page->filhos[pos];
+    //Filho esquerdo recebe o pai, e recebe um novo filho da sua página irmã.
+    filhoE->chaves[filhoE->nChaves] = page->chaves[pos];
+    filhoE->filhos[filhoE->nChaves + 1] = filhoD->filhos[0];
+    if(filhoD->filhos[0]){
+        filhoD->filhos[0]->pai = filhoE;
+    }
+    filhoE->nChaves++;
+    //Pai recebe a primeira chave do filho direito.
+    page->chaves[pos] = filhoD->chaves[0];
+    filhoD->nChaves--;
+    //Fazendo os elementos restantes do filho direito darem um shift para a esquerda.
+    for(int i = 0; i < filhoD->nChaves; i++){
+        filhoD->chaves[i] = filhoD->chaves[i+1];
+        filhoD->filhos[i] = filhoD->filhos[i+1];
+    }
+    filhoD->filhos[filhoD->nChaves] = filhoD->filhos[filhoD->nChaves + 1];
+    //Setando as últimas posições como nulo.
+    filhoD->chaves[filhoD->nChaves] = NULL;
+    filhoD->filhos[filhoD->nChaves + 1] = NULL;
+}
+
+/*
+Descrição: Função que realiza o processo de rotação para a direita na página.
+Entrada: Ponteiro para a página pai, inteiro indicando o filho direito.
+Saída: Nada.
+*/
+void emprestaEsquerda(pagina *page, int pos){
+    pagina *filhoD = page->filhos[pos], *filhoE = page->filhos[pos - 1];
+    //Fazendo os elementos do filho da direita abrirem espaço para o pai descer.
+    for(int i = filhoD->nChaves; i > 0; i--){
+        filhoD->chaves[i] = filhoD->chaves[i - 1];
+        filhoD->filhos[i+1] = filhoD->filhos[i];
+    }
+    filhoD->filhos[1] = filhoD->filhos[0];
+    //Elemento inicial do filho direito recebe o pai, e um novo filho da sua página irmã.
+    filhoD->chaves[0] = page->chaves[pos-1];
+    filhoD->filhos[0] = filhoE->filhos[filhoE->nChaves];
+    if(filhoE->filhos[filhoE->nChaves]){
+        filhoE->filhos[filhoE->nChaves]->pai = filhoD;
+    }
+    filhoD->nChaves++;
+    //Pai recebe a última chave do filho esquerdo.
+    page->chaves[pos-1] = filhoE->chaves[filhoE->nChaves-1];
+    filhoE->chaves[filhoE->nChaves - 1] = NULL;
+    filhoE->filhos[filhoE->nChaves] = NULL;
+    filhoE->nChaves--;
+}
+
+/*
+Descrição: Função que recebe a página pai, e mescla o filho da direita com o esquerdo. (Os elementos vão da direita para a esquerda).
+Entrada: Ponteiro para página pai, inteiro indicando o filho direito.
+Saída: Nada.
+*/
+void merge(pagina *page, int pos){
+    pagina *filhoE = page->filhos[pos-1], *filhoD = page->filhos[pos];
+    //Filho esquerdo recebe o pai correspondente, e um novo filho da sua página irmã.
+    filhoE->chaves[filhoE->nChaves] = page->chaves[pos-1];
+    filhoE->filhos[filhoE->nChaves+1] = filhoD->filhos[0];
+    filhoE->nChaves++;
+    //Filho esquerdo recebe os elementos da página irmã.
+    for(int i = 0; i < filhoD->nChaves; i++){
+        filhoE->chaves[filhoE->nChaves] = filhoD->chaves[i];
+        filhoE->filhos[filhoE->nChaves+1] = filhoD->filhos[i+1];
+        filhoE->nChaves++;
+    }
+    //Fazendo os elementos na página pai darem um shift para a esquerda.
+    for(int i = pos - 1; i < page->nChaves - 1; i++){
+        page->chaves[i] = page->chaves[i+1];
+        page->filhos[i+1] = page->filhos[i+2];
+    }
+    page->chaves[page->nChaves-1] = NULL;
+    page->filhos[page->nChaves] = NULL;
+    page->nChaves--;
+    //Vinculando todos os filhos da página irmã com o novo pai.
+    if(!filhoD->folha){
+        for(int i = 0; i <= filhoD->nChaves; i++){
+            filhoD->filhos[i]->pai = filhoE;
+        }
+    }
+    //Liberando a página irmã.
+    deletaPagina(filhoD);
+}
+
+/*
+Descrição: Função responsável por determinar qual dos métodos chamar para corrigir os filhos.
+Entrada: Ponteiro para a árvore (para ter acesso à ordem), ponteiro para a página pai, inteiro indicando a página filha que está com elementos abaixo do mínimo.
+Saída: Nada.
+*/
+void corrigiFilho(arvore *arv, pagina *page, int pos){
+    //Se o filho está na esquerda, o único empréstimo possível é da página irmã da direita.
+    if(pos == 0){
+        if(page->filhos[pos + 1]->nChaves > (arv->ordem/2) - 1){
+            //Página irmã consegue perder um elemento.
+            emprestaDireita(page, pos);
+        }else{
+            //Página irmã não consegue perder um elemento, unificando as duas.
+            merge(page, 1);
+        }
+    }else{
+        //Se o filho estiver na direita, o único empréstimo possível é da página irmã da esquerda.
+        if(pos == page->nChaves){
+            if(page->filhos[pos - 1]->nChaves > (arv->ordem/2) - 1){
+                //Página irmã consegue perder um elemento.
+                emprestaEsquerda(page, pos);
+            }else{
+                //Página irmã não consegue perder um elemento, unificando as duas.
+                merge(page, pos);
+            }
+        //Elemento está em uma página intermediária.
+        }else{
+            if(page->filhos[pos + 1]->nChaves > (arv->ordem/2) - 1){
+                //Página irmã da direita consegue perder um elemento.
+                emprestaDireita(page, pos);
+            }else if(page->filhos[pos - 1]->nChaves > (arv->ordem/2) - 1){
+                //Página irmã da esquerda consegue perder um elemento.
+                emprestaEsquerda(page, pos);
+            }else{
+                //Nenhuma das páginas irmãs conseguem perder um elemento, unificando com a página irmã da esquerda.
+                merge(page, pos);
+            }
+        }
+    }
+}
+
+/*
+Descrição: Função recursiva, responsável por procurar em qual filho está a chave ou em qual posição está chave.
+Entrada: Ponteiro para a árvore, ponteiro para a página, inteiro que será removido.
+Saída: 1 - Elemento encontrado e removido, 0 - Elemento não encontrado.
+*/
+int deletaDaPagina(arvore *arv, pagina *page, int mat) {
+    int pos, cond = 0;
+    if (page) {
+        //Procurando onde está a matrícula.
+        for(pos = 0; pos < page->nChaves && mat > page->chaves[pos]->chave; pos++);
+        //Verificando se a matrícula está na página atual.
+        if(pos < page->nChaves && mat == page->chaves[pos]->chave){
+            cond = 1;
+        }
+        //Se a matrícula estiver na página atual, removemos fazendo as verificações necessárias, se não estiver chamamos a essa mesma função recursivamente com o filho.
+        if (cond) {
+            //Se a página atual não for folha, copiamos o predecessor para o elemento atual, e removemos o com a chamada recursiva dessa função.
+            if (!page->folha) {
+                copiaPredecessor(page, pos);
+                cond = deletaDaPagina(arv, page->filhos[pos], page->chaves[pos]->chave);
+            //Página é folha, removendo elemento.
+            } else {
+                removeChave(page, pos);
+                arv->numElementos--;
+            }
+        } else {
+            //Elemento não está na página atual, verificando na página filha.
+            cond = deletaDaPagina(arv, page->filhos[pos], mat);
+        }
+        //Caso o filho onde foi removido um elemento estiver com um número de chaves menor do que o mínimo, devemos ajustar esse filho.
+        if (!page->folha) {
+            if (page->filhos[pos]->nChaves < (arv->ordem/2) - 1){
+                corrigiFilho(arv, page, pos);
+            }
+        }
+    }
+    return cond;
+}
+
+/*
+Descrição: Função responsável por verificar se a árvore está vazia, e por tratar os retornos da função deletaDaPagina.
+Entrada: Ponteiro para a árvore, inteiro que será removido.
+Saída: 1 - Sucesso, 0 - Elemento não encontrado, -1 - Árvore vazia.
+*/
+int deleta(arvore *arv, int mat){
+    pagina *aux, *raiz = arv->raiz;
+    //Verificando se a árvore está vazia.
+    if(arv->numElementos > 0){
+        //Se a função retornar 0, o elemento não foi encontrado.
+        if(!deletaDaPagina(arv, raiz, mat)){
+            return 0;
+        //Elemento encontrado e removido.
+        }else{
+            //Se a raiz atual possui 0 elementos, a altura da árvore diminuiu, e a raiz foi mesclada com o filho esquerdo.
+            if(raiz->nChaves == 0){
+                aux = raiz;
+                raiz = raiz->filhos[0];
+                if(raiz){
+                    raiz->pai = NULL;
+                }
+                deletaPagina(aux);
+                aux = NULL;
+            }
+        }
+        arv->raiz = raiz;
+        return 1;
+    }
+    return -1;
+}
